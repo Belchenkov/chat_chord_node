a9cb2dc0c13da71a874b8deccb0b7235f4e3f911
Aleksey Belchenkov <u608110@gmail.com>
2020-05-11 19:10:17 +0300

    Include Socket.io

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect
+io.on('connection', socket => {
+    console.log('New WS connection');
+
+    socket.emit('message', 'Welcome to ChatCord!')
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
